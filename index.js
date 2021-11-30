8185a6b379bb66ef2520010717491bb904e82134
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-11-30 13:42:21 -0800

    cards set the way i like, now need to figure out to make a scroll box for the p tag and fix the filter function

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
+document.addEventListener('DOMContentLoaded',() =>{
+  listeners()
+  fetchData()
+})
 
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+function likeCallback(e) {
+  const heart = e.target
+      if (heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = " "
+
+      }
+
+}
 const siteUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 const drinkCard = document.getElementById("all-drinks")
 const drinkList = document.getElementById('drink-list')
 const drinkStorrage =[]
 const renderDrinkListItem = (drink) => {
   const {strDrink, strCategory, strDrinkThumb, strIngredient1, strInstructions} = drink;
-
-
   const drinkLi = document.createElement("li");
-  // drinkLi.innerText = `${strDrink} : ${strCategory} : ${strIngredient1} : ${strInstructions}`;
   drinkLi.className = `drink ${strIngredient1.toLowerCase()}`;
 
   const drinkInstructions = document.createElement("p");
   drinkInstructions.innerText = strInstructions;
+
+  const likerButton = document.createElement('span')
+  likerButton.innerText = EMPTY_HEART;
+  likerButton.addEventListener("click", likeCallback );
+
 
   const drinkName = document.createElement("h4");
   drinkName.innerText = strDrink;
@@ -20,10 +40,7 @@ const renderDrinkListItem = (drink) => {
   const drinkImg = document.createElement("img");
   drinkImg.src = strDrinkThumb;
   drinkImg.className = "drink-image";
-
-
-
-  drinkLi.append(drinkImg,strDrink,drinkInstructions);
+  drinkLi.append(drinkImg,drinkName,drinkInstructions,likerButton);
 
   drinkCard.append(drinkLi);
 };
@@ -32,22 +49,21 @@ const renderDrinkListItem = (drink) => {
 
 
 // event handlers
-
-fetch(siteUrl)
-.then(res => res.json())
-.then(data => { 
-  data.drinks.forEach(drink =>{
-    drinkStorrage.push(drink)
-    renderDrinkListItem(drink)
-  
+function fetchData(){
+  fetch(siteUrl)
+  .then(res => res.json())
+  .then(data => { 
+    data.drinks.forEach(drink =>{
+      drinkStorrage.push(drink)
+      renderDrinkListItem(drink)
+    
+    })
   })
-})
-
+}
 
 const filterDrinks = (event) => {
 
-  const allDrinkNodes = document.querySelectorAll('.drink')
-  allDrinkNodes.forEach(drinkNode =>{
+  drinkStorrage.forEach(drinkNode =>{
     if(drinkNode.className.includes(event.target.id)) {
       drinkNode.style.display = 'row'
     }else{
@@ -57,8 +73,7 @@ const filterDrinks = (event) => {
 }
  
 const displayAllDrinks = () => {
-  const allDrinkNodes = document.querySelectorAll('.drink')
-  allDrinkNodes.forEach(drinkNode =>{
+  drinkStorrage.forEach(drinkNode =>{
     drinkNode.style.display = 'row'
    
   })
@@ -66,24 +81,24 @@ const displayAllDrinks = () => {
  
 
 
+function listeners() {
+  const vodkaBttn = document.getElementById('vodka')
+  vodkaBttn.addEventListener('click', filterDrinks)
 
-const vodkaBttn = document.getElementById('vodka')
-vodkaBttn.addEventListener('click', filterDrinks)
+  const ginBttn = document.getElementById('gin')
+  ginBttn.addEventListener('click', filterDrinks)
 
-const ginBttn = document.getElementById('gin')
-ginBttn.addEventListener('click', filterDrinks)
+  const rumBttn = document.getElementById('rum')
+  rumBttn.addEventListener('click', filterDrinks)
 
-const rumBttn = document.getElementById('rum')
-rumBttn.addEventListener('click', filterDrinks)
+  const whiskeyBttn = document.getElementById('whiskey')
+  whiskeyBttn.addEventListener('click', filterDrinks)
 
-const whiskeyBttn = document.getElementById('whiskey')
-whiskeyBttn.addEventListener('click', filterDrinks)
-
-const allDrinksBttn = document.getElementById('all-drink')
-allDrinksBttn.addEventListener('click', displayAllDrinks)
+  const allDrinksBttn = document.getElementById('all-drink')
+  allDrinksBttn.addEventListener('click', displayAllDrinks)
 
 
-
+}
 
  
 
